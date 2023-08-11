93d03d82890a33abf5f670db9ccc7f6b6ab403a4
InkoHX <me@inkohx.dev>
2023-08-11 14:50:13 +0900

    style: format code

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,6 @@ module.exports = {
   },
   plugins: ['@typescript-eslint'],
   rules: {
-    '@typescript-eslint/no-explicit-any': 'warn'
+    '@typescript-eslint/no-explicit-any': 'warn',
   },
 }
