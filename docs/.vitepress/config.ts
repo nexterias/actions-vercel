a3afaff3408705d8c4d2f10bdb97a8b099a27e0e
InkoHX <me@inkohx.dev>
2023-11-22 11:04:38 +0900

    style: format code

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: 'nexterias/actions-vercel',
-  description: 'Vercel deploy from GitHub Actions',
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    nav: [{ text: 'Home', link: '/' }],
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/nexterias/actions-vercel' },
-    ],
-  },
-})
+	title: "nexterias/actions-vercel",
+	description: "Vercel deploy from GitHub Actions",
+	themeConfig: {
+		// https://vitepress.dev/reference/default-theme-config
+		nav: [{ text: "Home", link: "/" }],
+		socialLinks: [
+			{ icon: "github", link: "https://github.com/nexterias/actions-vercel" },
+		],
+	},
+});
--- src/index.ts
+++ src/index.ts
@@ -1,76 +1,76 @@
-import * as core from '@actions/core'
-import * as github from '@actions/github'
-import * as vercel from './vercel'
-import * as input from './input'
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import * as vercel from "./vercel";
+import * as input from "./input";
 
 async function run() {
-  try {
-    const octokit = input.githubToken
-      ? github.getOctokit(input.githubToken)
-      : void 0
+	try {
+		const octokit = input.githubToken
+			? github.getOctokit(input.githubToken)
+			: void 0;
 
-    await vercel.install()
-    await vercel.pull()
-    if (input.isPrebuilt) await vercel.build()
-    const deploymentUrl = await vercel.deploy(octokit)
-    const { readyState } = await vercel.getDeployment(deploymentUrl)
-    if (input.domainAlias.length) await vercel.setAlias(deploymentUrl)
+		await vercel.install();
+		await vercel.pull();
+		if (input.isPrebuilt) await vercel.build();
+		const deploymentUrl = await vercel.deploy(octokit);
+		const { readyState } = await vercel.getDeployment(deploymentUrl);
+		if (input.domainAlias.length) await vercel.setAlias(deploymentUrl);
 
-    core.setOutput('deployment-url', deploymentUrl)
-    core.setOutput('deployment-status', readyState)
+		core.setOutput("deployment-url", deploymentUrl);
+		core.setOutput("deployment-status", readyState);
 
-    if (!octokit) return
+		if (!octokit) return;
 
-    const ref =
-      github.context.payload.pull_request?.head.sha ?? github.context.sha
-    const environment =
-      input.githubDeploymentEnvironment ?? input.isProduction
-        ? 'Production'
-        : 'Preview'
+		const ref =
+			github.context.payload.pull_request?.head.sha ?? github.context.sha;
+		const environment =
+			input.githubDeploymentEnvironment ?? input.isProduction
+				? "Production"
+				: "Preview";
 
-    // create Deployment
-    try {
-      const deployment = await octokit.rest.repos.createDeployment({
-        ...github.context.repo,
-        ref,
-        auto_merge: false,
-        environment,
-        required_contexts: [],
-      })
+		// create Deployment
+		try {
+			const deployment = await octokit.rest.repos.createDeployment({
+				...github.context.repo,
+				ref,
+				auto_merge: false,
+				environment,
+				required_contexts: [],
+			});
 
-      await octokit.rest.repos.createDeploymentStatus({
-        ...github.context.repo,
-        deployment_id: (deployment.data as { id: number }).id,
-        state: readyState === 'READY' ? 'success' : 'error',
-        environment_url: deploymentUrl,
-        auto_inactive: input.isProduction,
-      })
-    } catch (error: any) {
-      core.warning(
-        'May need "GITHUB_TOKEN" with "write" permission to "deployments"'
-      )
-      core.error(error)
-    }
+			await octokit.rest.repos.createDeploymentStatus({
+				...github.context.repo,
+				deployment_id: (deployment.data as { id: number }).id,
+				state: readyState === "READY" ? "success" : "error",
+				environment_url: deploymentUrl,
+				auto_inactive: input.isProduction,
+			});
+		} catch (error: any) {
+			core.warning(
+				'May need "GITHUB_TOKEN" with "write" permission to "deployments"',
+			);
+			core.error(error);
+		}
 
-    // create Commit Status
-    try {
-      await octokit.rest.repos.createCommitStatus({
-        ...github.context.repo,
-        sha: ref,
-        target_url: deploymentUrl,
-        state: readyState === 'READY' ? 'success' : 'error',
-        context: 'Vercel',
-        description: environment,
-      })
-    } catch (error: any) {
-      core.warning(
-        'May need "GITHUB_TOKEN" with "write" permission to "statuses"'
-      )
-      core.error(error)
-    }
-  } catch (error: any) {
-    core.setFailed(error)
-  }
+		// create Commit Status
+		try {
+			await octokit.rest.repos.createCommitStatus({
+				...github.context.repo,
+				sha: ref,
+				target_url: deploymentUrl,
+				state: readyState === "READY" ? "success" : "error",
+				context: "Vercel",
+				description: environment,
+			});
+		} catch (error: any) {
+			core.warning(
+				'May need "GITHUB_TOKEN" with "write" permission to "statuses"',
+			);
+			core.error(error);
+		}
+	} catch (error: any) {
+		core.setFailed(error);
+	}
 }
 
-run().catch((error) => core.setFailed(error))
+run().catch((error) => core.setFailed(error));
