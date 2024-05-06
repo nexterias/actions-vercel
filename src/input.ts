38b0c7f2e2b758ba321cb130f22a797edf6b4d65
InkoHX <me@inkohx.dev>
2024-05-07 00:16:29 +0900

    update

--- src/input.ts
+++ src/input.ts
@@ -1,6 +1,5 @@
 import * as core from "@actions/core";
-
-const environmentPattern = /^[A-Za-z][A-Za-z0-9_]*=(["']).+\1$/;
+import { parse as parseDotenv } from "dotenv";
 
 export const orgId = core.getInput("org-id", { required: true });
 export const projectId = core.getInput("project-id", { required: true });
@@ -13,11 +12,32 @@ export const domainAlias = core.getMultilineInput("domain-alias");
 export const githubToken = core.getInput("github-token") || void 0;
 export const githubDeploymentEnvironment =
 	core.getInput("github-deployment-environment") || void 0;
+export const buildEnvironments = parseDotenv(core.getInput("build-env"));
+export const environments = parseDotenv(core.getInput("env"));
 
-export const buildEnvironments = core
-	.getMultilineInput("build-env")
-	.filter((it) => environmentPattern.test(it));
+if (core.isDebug()) {
+	core.startGroup("Inputs");
+	core.debug(`orgId: ${orgId}`);
+	core.debug(`projectId: ${projectId}`);
+	core.debug(`token: ${token}`);
+	core.debug(`isProduction: ${isProduction}`);
+	core.debug(`isPrebuilt: ${isPrebuilt}`);
+	core.debug(`isPublic: ${isPublic}`);
+	core.debug(`cwd: ${cwd}`);
+	core.debug(`domainAlias: ${domainAlias}`);
+	core.debug(`githubToken: ${githubToken}`);
+	core.debug(`githubDeploymentEnvironment: ${githubDeploymentEnvironment}`);
 
-export const environments = core
-	.getMultilineInput("env")
-	.filter((it) => environmentPattern.test(it));
+	core.startGroup("buildEnvironments");
+	for (const [key, value] of Object.entries(buildEnvironments)) {
+		core.debug(`${key}=${value}`);
+	}
+	core.endGroup();
+
+	core.startGroup("environments");
+	for (const [key, value] of Object.entries(environments)) {
+		core.debug(`${key}=${value}`);
+	}
+	core.endGroup();
+	core.endGroup();
+}
